dc8fe8b3e1c213033cb9cae5fcfdab8fc7288851
Alexandr153 <boss191105@bk.ru>
2024-04-08 20:49:49 +0600

    Пользователь 1 завершил работу и отправил на проверку

--- Running out of space.py
+++ Running out of space.py
@@ -12,3 +12,4 @@ print(spacey(['kevin', 'has','no','space']))
 
 # test.assert_equals(spacey(['kevin', 'has','no','space']), [ 'kevin', 'kevinhas', 'kevinhasno', 'kevinhasnospace'])
 # test.assert_equals(spacey(['this','cheese','has','no','holes']), ['this','thischeese','thischeesehas','thischeesehasno','thischeesehasnoholes'])
+#ок
