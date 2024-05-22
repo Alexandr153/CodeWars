a8694ecdadf0e5aaf0be5a540a10e72bfdf7eb9c
Alexandr153 <boss191105@bk.ru>
2024-05-22 23:25:46 +0600

    Rework with task

--- Integers Recrecation One.py
+++ Integers Recrecation One.py
@@ -5,9 +5,12 @@ def list_squared(m: int, n: int) -> [int]:
     sortList: list = []
     for i in range(m, n+1):
         temp: list = []
-        for k in range(1, i+1):
+        for k in range(1, int(i**.5)+1):
             if i % k == 0:
+                div = i // k
                 temp.append(pow(k, 2))
+                if k != div:
+                    temp.append(pow(div, 2))
         temp: int = int(sum(temp))
 
         if sqrt(temp) % 1 == 0.0:
