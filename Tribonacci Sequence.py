d54911d122cbae2f3addc2b67744ef21dc0881b9
Alexandr Mitin <boss191105@bk.ru>
2024-09-07 21:01:03 +0600

    +1 задача

--- Tribonacci Sequence.py
+++ Tribonacci Sequence.py
@@ -1,4 +1,3 @@
 def tribonacci(signature: list[int], n: int) -> list:
-    [r for r in signature[:n]] if n <= 3 else [signature.append(sum(signature[:-4:-1])) for i in range(n - 3)]
-    return signature
-
+    res = [signature[r] for r in range(n)] if n < 3 else [signature.append(sum(signature[:-4:-1])) for i in range(n - 3)]
+    return res if n < 3 else signature
