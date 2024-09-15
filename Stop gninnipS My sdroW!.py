8bc1bfa1940da13c61e35db262af2aba83146002
Alexandr Mitin <boss191105@bk.ru>
2024-09-16 00:03:11 +0600

    +1 задача

---
+++ Stop gninnipS My sdroW!.py
@@ -0,0 +1,9 @@
+def spin_words(sentence: str) -> str:
+    sentence = sentence.split(' ')
+    for i, k in enumerate(sentence):
+        if len(k) >= 5:
+            sentence[i] = ''.join(reversed(k))
+
+    return ' '.join(sentence)
+
+
