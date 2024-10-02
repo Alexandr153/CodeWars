48993b8c6ad382ca31d7590380091ab874bb0d22
Alexandr Mitin <boss191105@bk.ru>
2024-10-03 01:51:10 +0600

    +1 задача

--- Playing with digits.py
+++ Playing with digits.py
@@ -1,10 +1,6 @@
-def dig_pow(n, p):
-    result = 0
+def dig_pow(n, p) -> int:
+    res = 0
     for i in str(n):
-        result += int(i)**p
+        res += int(i)**p
         p += 1
-    if result % n == 0:
-        return result / n
-    else:
-        return -1
-
+    return res / n if res % n == 0 else -1
